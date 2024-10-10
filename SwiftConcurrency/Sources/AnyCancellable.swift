0d01dc59173389b6105fcda349592e177b5a22f2
reza akbari <rezaakbariiosdeveloper@gmail.com>
2024-10-10 16:01:55 +0330

    feat anyCancellable exapmle

---
+++ SwiftConcurrency/Sources/AnyCancellable.swift
@@ -0,0 +1,32 @@
+ final class AnyCancellable: Hashable {
+
+    private let cancel: () -> Void
+
+    init(_ cancel: @escaping () -> Void) {
+        self.cancel = cancel
+    }
+
+    deinit {
+        cancel()
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+
+    static func == (lhs: AnyCancellable, rhs: AnyCancellable) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+ }
+
+ extension AnyCancellable {
+    func store(_ store: inout Set<AnyCancellable>) {
+        store.insert(self)
+    }
+ }
+
+ extension Task {
+    func store(_ store: inout Set<AnyCancellable>) {
+        store.insert(AnyCancellable(cancel))
+    }
+ }
